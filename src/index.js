498931ccc66c0856b3fdf5cc555b5c905714f630
Taras <protsyshyn.taras@coaxsoft.com>
2022-09-10 13:15:36 +0300

    add express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 5005;
+
+app.get("/", (req, res) => {
+  res.send("Hello there!");
+});
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
